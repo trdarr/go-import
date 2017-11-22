1f786dfda374bacad3cc87edee033c4090638031
Thomas Darr <me@thomasd.se>
2017-11-22 23:34:48 +0100

    Another initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM golang:1.9-alpine AS build
+
+WORKDIR /go/src/app
+RUN mkdir -p /go/src/app
+COPY index.gohtml main.go ./
+RUN go build
+
+
+FROM alpine
+
+WORKDIR /app
+COPY --from=build /go/src/app .
+ENTRYPOINT ./app
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const port = "8080"
+
+func main() {
+	t, err := template.ParseFiles("index.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		pkg := strings.TrimPrefix(r.URL.Path, "/")
+		if pkg == "" {
+			return
+		}
+
+		t.Execute(w, struct {
+			Pkg string
+			URI string
+		}{pkg, fmt.Sprintf("https://github.com/trdarr/%s", pkg)})
+	})
+
+	log.Printf("Listening on port %s.\n", port)
+	log.Fatalln(http.ListenAndServe(":"+port, nil))
+}
